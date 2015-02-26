edf1cb74ead4438da8fb35a46b017d67279e19c6
Shimran George & Nathan Rohde <student@epicodus.com>
2015-02-26 10:28:07 -0800

    Add Upvote/Downvote functionality

--- controllers/linksController.js
+++ controllers/linksController.js
@@ -1,9 +1,16 @@
 hackerNewsList.controller('LinksCtrl', function LinksCtrl($scope, $stateParams) {
   $scope.webLinks = [];
   $scope.addWebLink = function() {
-    $scope.webLinks.push({name: $scope.webLinkName, url: $scope.webLinkUrl, id: $scope.webLinks.length + 1});
+    $scope.webLinks.push({name: $scope.webLinkName, url: $scope.webLinkUrl, vote: 1, id: $scope.webLinks.length + 1});
   };
   $scope.webLinkName = null;
   $scope.webLinkURL = null;
+  $scope.upVote = function(webLink){
+    webLink.vote += 1;
+  };
+  $scope.downVote = function(webLink){
+    webLink.vote -=1;
+  };
+
 
 });
